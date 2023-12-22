d38088dd6cd8ff1294d7362bc23f971a573194d8
KIRTANA KIRAN <122041511+kirtanakiran@users.noreply.github.com>
2023-12-22 12:35:26 +0530

    Update Sailors.sql

--- Sailors.sql
+++ Sailors.sql
@@ -23,3 +23,23 @@ CREATE TABLE RESERVERS(
     FOREIGN KEY(bid)REFERENCES BOAT(bid)
    );
 
+insert into SAILORS values
+(1,"Albert", 5.0, 40),
+(2, "Nakul", 5.0, 49),
+(3, "Darshan", 9, 18),
+(4, "Astorm Gowda", 2, 68),
+(5, "Armstormin", 7, 19);
+
+ insert into BOAT values
+(1,"Boat_1", "Green"),
+(2,"Boat_2", "Red"),
+(103,"Boat_3", "Blue");
+
+insert into RESERVERS values
+(1,103,"2023-01-01"),
+(1,2,"2023-02-01"),
+(2,1,"2023-02-05"),
+(3,2,"2023-03-06"),
+(5,103,"2023-03-06"),
+(1,1,"2023-03-06");
+
